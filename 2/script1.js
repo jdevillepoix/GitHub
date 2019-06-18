606d759d8cb50fb364aebfd22279456e97b47a54
Jeanne de Villepoix <jdevillepoix@hotmail.fr>
2019-06-18 16:55:15 +0200

    Final 18/06

--- 2/script1.js
+++ 2/script1.js
@@ -2,7 +2,6 @@ $( document ).ready(function welcome() {
 	alert("bienvenue")
    $("#welcome").html('<h4>Bienvenue, <br> soyez prêt à renouveler de nouveaux défis !</h4>');
 
-
 function getUsername() {
 
     var  user = prompt("Veuillez rentrez votre nom:", "");
@@ -12,10 +11,44 @@ function getUsername() {
 
  function welcome() {
     var name= getUsername();
-    $("#welcome").html('<h4>Bienvenue '+name+', <br> soyez pret à renouveler de nouveaux défis ! </h4>');
+    $("#welcome").html('<h4>Bienvenue '+name+', <br> soyez prêt à renouveler de nouveaux défis ! </h4>');
 
 };
 
 welcome();
+
+function loadChallenges() {
+  $.ajax(
+        {url : 'https://s3.eu-central-1.amazonaws.com/spintransfer/challenge.json',
+        type: 'GET',
+        dataType: 'json'} 
+  )
+  .done(function(data) {
+    showChallenge(data);
+  })
+  .fail(function() {
+    alert( "error" );
+  });
+}
+
+function showChallenge(liste) {
+
+  var data="";
+
+  for ( var i=0; i < liste.length; i++) {
+    data+='<div class="defi">';
+
+    var challenge=liste[i];
+
+    data+='<h2>'+challenge.nom+'</h2>';
+    data+='<p>'+challenge.description+'</p>';
+    data+='<iframe src="'+challenge.youtube+'" allowfullscreen="" width="364" height="204" frameborder="0"></iframe>'
+    data+="</div>";
+  }
+  $("#defis").html(data);
+
+};
+
+loadChallenges();
 
 });
