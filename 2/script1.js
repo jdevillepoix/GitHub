b3dae88c2c93cf9322985ea176df6f71b8b928d7
Jeanne de Villepoix <jdevillepoix@hotmail.fr>
2019-06-18 16:47:32 +0200

    3

--- 2/script1.js
+++ 2/script1.js
@@ -1,4 +1,21 @@
 $( document ).ready(function welcome() {
 	alert("bienvenue")
    $("#welcome").html('<h4>Bienvenue, <br> soyez prêt à renouveler de nouveaux défis !</h4>');
+
+
+function getUsername() {
+
+    var  user = prompt("Veuillez rentrez votre nom:", "");
+ 
+    return user
+}
+
+ function welcome() {
+    var name= getUsername();
+    $("#welcome").html('<h4>Bienvenue '+name+', <br> soyez pret à renouveler de nouveaux défis ! </h4>');
+
+};
+
+welcome();
+
 });
